c93b88f1128d9d4a3aa77408a20f13fa3c1bc535
Vincent Opurum <vincentchimene@gmail.com>
2023-10-17 18:28:14 +0100

    updated the base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines all common attributes/methods for other classes
+"""Defines the general attributes/methods for other classes
 """
 import uuid
 from datetime import datetime
@@ -32,7 +32,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)  # as instructed also in task 5
+            models.storage.new(self)  # for task 5
 
     def __str__(self):
         """Returns a readable string representation
@@ -42,7 +42,7 @@ class BaseModel:
         return "[{}] ({}) {}".format(clsName, self.id, self.__dict__)
 
     def save(self):
-        """Updates the public instance attribute
+        """Updates the public instance attribute updated_at
         with the current datetime"""
 
         self.updated_at = datetime.now()
@@ -50,7 +50,7 @@ class BaseModel:
 
     def to_dict(self):
         """Returns a dictionary that contains all
-        keys/value"""
+        keys/values of the instance"""
         my_dict = self.__dict__.copy()
         my_dict['updated_at'] = self.updated_at.isoformat()
         my_dict['created_at'] = self.created_at.isoformat()
